3636d8dfa055b11eb76fadd1df1117b9a151b9ab
Milton Cuervo <milton.cuervo@udea.edu.co>
2026-03-11 16:10:36 -0500

    Revision final

--- lab12025p/src/test/java/com/udea/lab12025p/cucumber/CucumberTest.java
+++ lab12025p/src/test/java/com/udea/lab12025p/cucumber/CucumberTest.java
@@ -1,7 +1,5 @@
 package com.udea.lab12025p.cucumber;
 
-import io.cucumber.junit.platform.engine.Constants;
-import org.junit.platform.suite.api.ConfigurationParameter;
 import org.junit.platform.suite.api.IncludeEngines;
 import org.junit.platform.suite.api.SelectClasspathResource;
 import org.junit.platform.suite.api.Suite;
@@ -10,5 +8,6 @@ import org.junit.platform.suite.api.Suite;
 @IncludeEngines("cucumber")
 @SelectClasspathResource("features")
 public class CucumberTest {
-    // Clase vacía, sirve como Switch maestro de JUnit Platform
+    // Esta clase solo sirve como punto de entrada para JUnit 5 Suite.
+    // La configuración detallada está en junit-platform.properties
 }
