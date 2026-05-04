f0bc4102282917bde8a6095c664ed9ce953df1cc
Santiago Zapata <santiagod.zapata@udea.edu.co>
2026-05-04 17:08:38 -0500

    Refactor: Wildcard Substitution

--- lab12025p/src/main/java/com/udea/lab12025p/controller/TransactionController.java
+++ lab12025p/src/main/java/com/udea/lab12025p/controller/TransactionController.java
@@ -25,12 +25,12 @@ public class TransactionController {
     }
 
     @PostMapping
-    public ResponseEntity<?> transferMoney(@RequestBody TransactionDTO transactionDTO) {
+    public ResponseEntity<TransactionDTO> transferMoney(@RequestBody TransactionDTO transactionDTO) {
         try {
             TransactionDTO savedTransaction = transactionService.transferMoney(transactionDTO);
             return ResponseEntity.ok(savedTransaction);
         } catch (IllegalArgumentException e) {
-            return ResponseEntity.badRequest().body(e.getMessage());
+            return ResponseEntity.badRequest().build();
         }
     }
 
