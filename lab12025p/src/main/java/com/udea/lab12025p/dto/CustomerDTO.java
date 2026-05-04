17af56b5bcb4ed04378e51649ebb924661d6ded9
MariAgudelo2 <maria.aagudelo@udea.edu.co>
2026-05-04 18:28:38 -0500

    Refactor: simplify DTOs using Lombok annotations

--- lab12025p/src/main/java/com/udea/lab12025p/dto/CustomerDTO.java
+++ lab12025p/src/main/java/com/udea/lab12025p/dto/CustomerDTO.java
@@ -1,7 +1,15 @@
 package com.udea.lab12025p.dto;
 
 import java.math.BigDecimal;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class CustomerDTO {
 
     private Long id;
@@ -10,54 +18,4 @@ public class CustomerDTO {
     private String accountNumber;
     private BigDecimal balance;
 
-    public CustomerDTO() {
-    }
-
-    public CustomerDTO(Long id, String firstName, String lastName, String accountNumber, BigDecimal balance) {
-        this.id = id;
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.accountNumber = accountNumber;
-        this.balance = balance;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getAccountNumber() {
-        return accountNumber;
-    }
-
-    public void setAccountNumber(String accountNumber) {
-        this.accountNumber = accountNumber;
-    }
-
-    public BigDecimal getBalance() {
-        return balance;
-    }
-
-    public void setBalance(BigDecimal balance) {
-        this.balance = balance;
-    }
 }
--- lab12025p/src/main/java/com/udea/lab12025p/dto/TransactionDTO.java
+++ lab12025p/src/main/java/com/udea/lab12025p/dto/TransactionDTO.java
@@ -1,10 +1,17 @@
 package com.udea.lab12025p.dto;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
-
 import java.time.LocalDateTime;
 import java.math.BigDecimal;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class TransactionDTO {
 
@@ -14,55 +21,4 @@ public class TransactionDTO {
     private BigDecimal amount;
     private LocalDateTime transactionDate;
 
-    public TransactionDTO() {
-    }
-
-    public TransactionDTO(Long id, String senderAccountNumber, String receiverAccountNumber, BigDecimal amount,
-            LocalDateTime transactionDate) {
-        this.id = id;
-        this.senderAccountNumber = senderAccountNumber;
-        this.receiverAccountNumber = receiverAccountNumber;
-        this.amount = amount;
-        this.transactionDate = transactionDate;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getSenderAccountNumber() {
-        return senderAccountNumber;
-    }
-
-    public void setSenderAccountNumber(String senderAccountNumber) {
-        this.senderAccountNumber = senderAccountNumber;
-    }
-
-    public String getReceiverAccountNumber() {
-        return receiverAccountNumber;
-    }
-
-    public void setReceiverAccountNumber(String receiverAccountNumber) {
-        this.receiverAccountNumber = receiverAccountNumber;
-    }
-
-    public BigDecimal getAmount() {
-        return amount;
-    }
-
-    public void setAmount(BigDecimal amount) {
-        this.amount = amount;
-    }
-
-    public LocalDateTime getTransactionDate() {
-        return transactionDate;
-    }
-
-    public void setTransactionDate(LocalDateTime transactionDate) {
-        this.transactionDate = transactionDate;
-    }
 }
